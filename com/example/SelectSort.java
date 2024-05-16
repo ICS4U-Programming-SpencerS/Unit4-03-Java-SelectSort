38363430a854de0b4f7955aa57e7ee1b9f865ebd
PencersGitHub <113054133+PencersGitHub@users.noreply.github.com>
2024-05-16 10:15:59 -0400

    Initial commit

---
+++ com/example/SelectSort.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Spencer Scarlett
+ * @version 1.0
+ * @since 2024-05-16
+ */
+
+// SelectSort class
+public final class SelectSort {
+
+    /** Private constructor to prevent instantiation. */
+    private SelectSort() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
